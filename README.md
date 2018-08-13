0df6582cc25f5540316165fdd38f25f7b97e7756
Edwin <edwin@kenzie.academy>
2018-08-13 10:49:43 -0400

    Finished readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Test These Katas
+[katas.py](katas.py) includes our solution to a [previous set of katas](https://my.kenzie.academy/courses/2/assignments/853?module_item_id=992)
+assessment.
+
+Unfortunately, while we had good intentions and wrote a [test
+harness](./tests), we didn't actually write any unit tests! 
+
+## Getting Started
+There are several ways to start this activity, but we suggest that you start
+by running the unit tests in a separate terminal (like the one built into VS
+Code) using `rerun`:
+
+```console
+foo@bar:~ $ rerun "python -m unittest discover"
+```
+
+You should see something like the following:
+
+![test harness output](./screenshot.png)
+
+Then, open up [tests/test_katas.py](./tests/test_katas.py) and start fixing those TODOs!
---
+++ katas.py
@@ -0,0 +1,49 @@
+def add(x, y):
+    """ Return the result of adding x to y. """
+    return x + y
+
+
+def multiply(x, y):
+    """ Return the result of multiplying x by y. """
+    result = 0
+
+    for _ in range(x):
+        result = add(result, y)
+
+    return result
+
+
+def power(x, n):
+    """ Return the result of taking x to the nth power. """
+    result = 1
+
+    for _ in range(n):
+        result = multiply(result, x)
+
+    return result
+
+
+def factorial(n):
+    """ Return the result of calculating the factorial of n. """
+    result = 1
+
+    for i in range(1, n + 1):
+        result = multiply(result, i)
+
+    return result
+
+
+def fibonacci(n):
+    """ Return the nth number in the fibonacci sequence (starting at 0). """
+    if n < 3:
+        return n - 1
+    else:
+        second_to_last = 0
+        last = 1
+        result = 1
+
+        for _ in range(2, n):
+            result = second_to_last + last
+            second_to_last, last = last, result
+
+        return result
